36188ae35f492a79582e7de3f17a2f2302fd36c0
Dipayan Dey <162149304+dipayan-2004@users.noreply.github.com>
2024-08-21 16:04:46 +0000

    commit

---
+++ javaScript/variable.js
@@ -0,0 +1,13 @@
+let a=2;
+//let a=3;
+// this is why it is more efficient..
+//bcz it can't be re-declared
+
+var b=32;
+var b="world";
+// this is why var keyword does not used..
+
+console.log(a);
+console.log(b);
+console.table([a,b]);
+
